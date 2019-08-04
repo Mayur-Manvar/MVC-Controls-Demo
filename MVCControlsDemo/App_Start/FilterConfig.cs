9aa2ca87b72d9731a48a140d767f81e97bb7063a
Mayur-Manvar <mayurpmanvar@gmail.com>
2019-08-04 17:17:09 +0530

    Add project files.

---
+++ MVCControlsDemo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVCControlsDemo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
